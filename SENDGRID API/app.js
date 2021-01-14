8d2f4d10beeda2ef0cb74f16c1142a19d192ad02
Nishita5421 <nishipal121@gmail.com>
2021-01-14 19:04:38 +0530

    automated send email

--- SENDGRID API/app.js
+++ SENDGRID API/app.js
@@ -1,19 +1,52 @@
+
+var express=require("express");
+var app=express();
+var path = require('path');
+var bodyParser=require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 const sgMail = require('@sendgrid/mail');
 const API_KEY='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';
+app.get("/",function(req,res)
+{
+res.render("email.ejs");
+});
 
-sgMail.setApiKey(API_KEY);
+app.post("/",function(req,res){
+  var frome=req.body.from;
+  var toe=req.body.to;
+  
+  var subjecte=req.body.subject;
+  var msge=req.body.msgs;
+  sgMail.setApiKey(API_KEY);
 const msg = {
-  to: 'kaustubh.ag5@gmail.com', // Change to your recipient
-  from: 'nishipal121@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js ,I did it with SENDGRID API if you are reading this then my project is complted ;)',
-  html: '<strong>and easy to do anywhere, even with Node.js ,I did it with SENDGRID API if you are reading this then my project is complted ;)</strong>',
+  to: toe, // Change to your recipient
+  from: frome, // Change to your verified sender
+  subject: subjecte,
+  //text: 'and easy to do anywhere, even with Node.js ,I did it with SENDGRID API if you are reading this then my project is complted ;)',
+  html: msge,
 }
 sgMail
   .send(msg)
   .then(() => {
     console.log('Email sent')
+    res.render("thankyou.ejs",{thankyou:'Email sent',error:null});
+    
+ 
+    
   })
   .catch((error) => {
     console.error(error)
+    res.render("thankyou.ejs",{thankyou:'null',error:'There was some error!'});
+ 
   })
+  
+
+
+});
+app.listen(4000,function(){
+    console.log("Server has started");
+})
+
+
